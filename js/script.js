a4c5bd66ec4ffdb5319e8c9c05da73a9892596ce
mgbc2022 <ljwmg333@gmail.com>
2022-11-15 22:28:35 +0900

    막대 그래프

--- js/script.js
+++ js/script.js
@@ -12,3 +12,25 @@ var pieOptions = {
 };
 
 new Chartist.Pie(".pie-chart", pieData, pieOptions);
+
+/*
+BAR CHART
+================================================ */
+var barData = {
+  labels: ["2018년", "2019년", "2020년"],
+  series: [[10, 16, 29]],
+};
+
+var barOptions = {
+  axisY: {
+    offset: 60,
+    scaleMinSpace: 50,
+    labelInterpolationFnc: function (value) {
+      return value + " 명";
+    },
+  },
+  width: "100%",
+  height: "400px",
+};
+
+new Chartist.Bar(".bar-chart", barData, barOptions);
